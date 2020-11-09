0b3a6e0d7acc9dbae8685d196a9bcbdfca9d34d3
Kate Boyd <kwboyd95@gmail.com>
2020-11-09 09:48:06 -0700

    Push plugin version

---
+++ content.js
@@ -0,0 +1,16 @@
+(function() {
+    setTimeout(() => {
+        const elementToObserve = document.querySelector(`[aria-label="Timeline: Your Home Timeline"]`)
+    
+        const callback = function(mutationsList) {
+            const nodes = elementToObserve.querySelectorAll(`[lang="en"]`)
+            for (node of nodes) {
+                node.textContent = "Nope."
+            }
+        }
+    
+        const tweetObserver = new MutationObserver(callback);
+    
+        tweetObserver.observe(elementToObserve, {subtree: true, childList: true});
+    }, 1000)
+})()
